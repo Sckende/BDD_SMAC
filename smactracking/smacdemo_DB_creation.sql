a50f2d47c13044b581df91f7dc88ee794d2cbb8b
Sckende <clrccljuhasz@gmail.com>
2022-11-03 15:27:48 +0400

    # codage dev

--- smactracking/smacdemo_DB_creation.sql
+++ smactracking/smacdemo_DB_creation.sql
@@ -1,6 +1,6 @@
 -- List creation
-    -- demo::ge
-CREATE TYPE ge_list
+    -- demo::type_piege
+CREATE TYPE type_piege_list
 AS ENUM ('a la main',
          'au nid',
          'balchatri',
@@ -24,8 +24,8 @@ AS ENUM ('a la main',
          'wooshnet',
          'autre');
 
-    -- demo::fs
-CREATE TYPE fs_list
+    -- demo::effort_capture
+CREATE TYPE effort_capture_list
 AS ENUM ('a la main',
          'au nid',
          'balchatri',
@@ -59,7 +59,230 @@ AS ENUM ('ACRTRI',
          'PYCJOC',
          'STEFUS',
          'STRPIC',
-         'ZOSBOR')
+         'ZOSBOR');
+
+    -- nid_metadatas::categorie_nid
+CREATE TYPE categorie_nid_list
+AS ENUM ('NPO',
+         'NPB',
+         'NOC');
+
+    -- nid_metadatas::type_nid
+CREATE TYPE type_nid_list
+AS ENUM ('TH',
+         'TSB',
+         'THSB',
+         'CR');
+
+    -- nid_metadatas::condition_nid
+CREATE TYPE condition_nid_list
+AS ENUM ('OK',
+         'EFF1',
+         'EFF2',
+         'REHAB',
+         'STOP_SUIVI');
+
+    -- nid_visites::statut_nid
+CREATE TYPE statut_nid_list
+AS ENUM ('TPROF',
+         'OCC',
+         'VIDE',
+         'NRT',
+         'NC');
+
+    -- nid_visites::contenu_nid
+CREATE TYPE contenu_nid_list
+AS ENUM ('AUCUN',
+         'INCONNU',
+         'O',
+         'ONV',
+         'OINAX',
+         'PUL',
+         'PULINAX',
+         'PULDEAD',
+         'AD',
+         '2AD',
+         'ADINAX',
+         'INAX',
+         'ADDEAD');
+
+    -- nid_visites::methode_contenu
+CREATE TYPE methode_contenu_list
+AS ENUM ('MAIN',
+         'BURROWSCOPE',
+         'REGLE');
+
+    -- demo::origine_data
+CREATE TYPE origine_data_list
+AS ENUM ('COLONIE',
+         'SEOR',
+         'PEQ_LA_KOUR');
+
+    -- demo::id_prog
+CREATE TYPE id_prog_list
+AS ENUM ('609',
+         '616');
+
+    -- manip_oiseaux::bird_action
+CREATE TYPE bird_action_list
+AS ENUM ('B',
+         'C',
+         'R',
+         'BREDOUILLE',
+         'SANS',
+         'PERDUE',
+         'DETRUITE');
+
+    -- manip_oiseaux::bird_age
+CREATE TYPE bird_age_list
+AS ENUM ('PUL',
+         '1A',
+         '2A',
+         '1A?',
+         '2A?',
+         '+1A',
+         '+2A',
+         '+1?',
+         '+2?',
+         'VOL');
+
+    -- manip_oiseaux::etat_sante
+CREATE TYPE etat_sante_list
+AS ENUM ('0',
+         'BLESS_ANCIEN',
+         'BLESS_MARQUE',
+         'PLUME_VOL',
+         'ENVOL',
+         'BLESS_LEG',
+         'LANGUE',
+         'STRESS',
+         'BLESS_MOD',
+         'BLESS_SEV',
+         'MORT'); -- peut être null
+
+    -- manip_oiseaux::etat_mue
+CREATE TYPE etat_mue_list
+AS ENUM ('J',
+         '0',
+         'C',
+         'Q',
+         'R'); -- peu être null
+
+    -- baguages::bird_sexe
+CREATE TYPE bird_sexe_list
+AS TYPE ('?',
+         'F',
+         'M',
+         'F?',
+         'M?');
+
+    -- baguages::methode_sexage
+CREATE TYPE methode_sexage_list
+AS TYPE ('genetique',
+         'phenotypique',
+         'sanguin',
+         'autres');
+
+    -- reprises::cond_repr
+CREATE TYPE cond_repr_list
+AS ENUM ('0',
+         '1',
+         '2',
+         '3',
+         '4',
+         '5',
+         '6',
+         '7',
+         '8',
+         '9');
+
+    -- reprises::cirs_repr
+CREATE TYPE circ_repr_list
+AS ENUM ('99',
+         '00',
+         '01',
+         '02',
+         '03',
+         '06',
+         '07',
+         '08',
+         '09',
+         '10',
+         '11',
+         '12',
+         '13',
+         '14',
+         '15',
+         '16',
+         '19',
+         '20',
+         '21',
+         '22',
+         '23',
+         '24',
+         '25',
+         '26',
+         '27',
+         '28',
+         '30',
+         '31',
+         '32',
+         '33',
+         '34',
+         '35',
+         '36',
+         '37',
+         '38',
+         '40',
+         '41',
+         '42',
+         '43',
+         '44',
+         '45',
+         '46',
+         '47',
+         '48',
+         '49',
+         '91',
+         '50',
+         '51',
+         '52',
+         '53',
+         '54',
+         '55',
+         '56',
+         '57',
+         '58',
+         '59',
+         '60',
+         '61',
+         '62',
+         '63',
+         '64',
+         '65',
+         '66',
+         '67',
+         '68',
+         '69',
+         '70',
+         '71',
+         '72',
+         '73',
+         '74',
+         '75',
+         '76',
+         '77',
+         '78',
+         '80',
+         '81',
+         '82',
+         '83',
+         '84',
+         '85',
+         '86',
+         '87',
+         '88',
+         '89');
 
 -- Table creation
 CREATE TABLE localites (
@@ -118,14 +341,14 @@ CREATE TABLE missions_protocoles (
 -- Table parent
 CREATE TABLE demo (
     lieudit_id INT REFERENCES lieudits (lieudit_id) 
-    prog_id prog_id_list, -- CRBPO
-    centre centre_list, -- CRBPO
-    theme_session "PROG PERS", -- CRBPO
-    theme "PROG PERS", -- CRBPO
-    origine_data origine_data_list, -- need to CREATE TYPE
-    ge ge_list, -- CRBPO
-    fs fs_list, -- CRBPO
-    espece espece_list -- CRBPO, need to CREATE TYPE ou CREATE TABLE?
+    id_prog id_prog_list, -- CRBPO
+    centre DEFAULT "FRP", -- CRBPO
+    theme_session DEFAULT "PROG PERS", -- CRBPO
+    theme DEFAULT "PROG PERS", -- CRBPO
+    origine_data origine_data_list,
+    type_piege type_piege_list, -- CRBPO
+    effort_capture effort_capture_list, -- CRBPO
+    espece espece_list -- CRBPO
     bagueur  INT REFERENCES intervenants (intervenant_id) -- CRBPO
     
 );
@@ -141,10 +364,10 @@ CREATE TABLE manip_oiseaux (
     pds FLOAT,
     plaque_incub BOOLEAN,
     age bird_age_list, -- CRBPO, need to CREATE TYPE
-    es etat_sante_list, -- CRBPO,  need to CREATE TYPE
-    mu etat_mue_list, -- CRBPO,  need to CREATE TYPE
-    bg INT REFERENCES intervenants (intervenant_id), -- CRBPO, TO CHECK
-    sg INT REFERENCES intervenants (intervenant_id), -- CRBPO, TO CHECK
+    etat_sante etat_sante_list, -- CRBPO,  need to CREATE TYPE
+    etat_mue etat_mue_list, -- CRBPO,  need to CREATE TYPE
+    bagueur_vrai INT REFERENCES intervenants (intervenant_id), -- CRBPO, TO CHECK
+    stagiaire_formation INT REFERENCES intervenants (intervenant_id), -- CRBPO, TO CHECK
     remarque_manip TEXT
 );
 
@@ -156,9 +379,9 @@ CREATE TABLE nid_metadatas (
     hauteur_initiale FLOAT,
     largeur_initiale FLOAT,
     profondeur_initiale FLOAT,
-    categorie_nid categorie_nid_list, -- need to CREATE TYPE
-    type_nid type_nid_list, -- need to CREATE TYPE
-    condition_nid condition_nid_list, -- need to CREATE TYPE
+    categorie_nid categorie_nid_list, 
+    type_nid type_nid_list,
+    condition_nid condition_nid_list, 
     intervenant_id INT REFERENCES intervenants (intervenant_id),
     remarque_nid TEXT    
 );
@@ -168,8 +391,8 @@ CREATE TABLE nid_visites (
     nid_metadata_id INT REFERENCES nid_metadatas (nid_metadata_id),
     intervenant_id INT REFERENCES intervenants (intervenant_id),
     manip_oiseau_id INT REFERENCES manip_oiseaux (manip_oiseau_id),
-    contenu_nid contenu_list_nid, -- need to CREATE TYPE
-    methode_contenu methode_contenu_list -- need to CREATE TYPE
+    contenu_nid contenu_nid_list, 
+    methode_contenu methode_contenu_list 
 );
 
 CREATE TABLE baguages (
@@ -181,7 +404,7 @@ CREATE TABLE baguages (
     cr FLOAT, -- TO CONFIRME
     bh FLOAT, -- TO CONFIRME
     morpho_complete BOOLEAN,
-    sexe oiseau_sexe_list, -- CRBPO, NEED TO CREATE TYPE
+    sexe bird_sexe_list, -- CRBPO, NEED TO CREATE TYPE
     methode_sexage methode_sexage_list, -- NEED TO CREATE TYPE
     groupe_genetique -- TO DEFINE
 );
@@ -196,12 +419,12 @@ CREATE TABLE reprises (
 CREATE TABLE oiseau_soins (
     oiseau_soin_id BIGSERIAL NOT NULL PRIMARY KEY,
     manip_oiseau_id INT REFERENCES manip_oiseaux (manip_oiseau_id),
-    localite_relache INT REFERENCES localites (localite_id), -- NEED TO CREATE TABLE
+    localite_relache INT REFERENCES localites (localite_id), 
     date_decouverte DATE, -- voir si equivalent a la date de baguage
     date_relache DATE,
-    un un_list, -- CRBPO, NEED TO CREATE TYPE, toujours le meme ?
-    MEMO TEXT, -- CRBPO, a garder ?
-    ir TEXT, -- CRBPO, a garder ?
+    centre_soins DEFAULT "valeur_unique", -- CRBPO, toujours le meme ?
+    memo TEXT, -- CRBPO, a garder ?
+    couleir_iris TEXT, -- CRBPO, a garder ?
     type_marque ??, -- CRBPO, a garder ?
     couleur_gauche ??, -- CRBPO, a garder ?
     couleur_droite ??, -- CRBPO, a garder ?
